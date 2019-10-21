99d31ab63fc9a9783e7345460e73192678c64913
Reint <33000433+reint-fischer@users.noreply.github.com>
2019-10-21 18:03:26 +0200

    Inversemodel

--- Code/realemissions.py
+++ Code/realemissions.py
@@ -12,6 +12,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from oneboxmodel import oneboxeulerfw, oneboxRK4
 from twoboxmodel import twoboxeulerfw
+from inversemodel import inverseonebox
 
 P_WMO2014_df=pd.read_excel('emissions_2014.xlsx')
 P_WMO2018_df=pd.read_excel('agage_emissions.xlsx')
@@ -48,16 +49,21 @@ Errormax=(P_WMO2018_df[substanceunits]+P_WMO2018_df['Uncertainties ' + substance
 Errormax=np.concatenate((P_WMO2014[:29],Errormax[1:]))
 Errormin=(P_WMO2018_df[substanceunits]-P_WMO2018_df['Uncertainties ' + substanceunits]).to_numpy()/1000
 Errormin=np.concatenate((P_WMO2014[:29],Errormin[1:]))
-Errormax=oneboxRK4(k,Errormax,C0,dt,MW)*1.07
-Errormin=oneboxRK4(k,Errormin,C0,dt,MW)*1.07
+Errormax=oneboxRK4(k,Errormax,C0,dt,MW)*1.06
+Errormin=oneboxRK4(k,Errormin,C0,dt,MW)*1.06
 
 ObsErrormax = O_df[substance]+O_df['---'+substance_df.iloc[2][substance]]
 ObsErrormin = O_df[substance]-O_df['---'+substance_df.iloc[2][substance]]
 
+#Inverse ERRORBARS
+Invmax = inverseonebox(1/67,O_df[substance]/(10**12),dt/12,MW)
+Invmin = inverseonebox(1/43,O_df[substance]/(10**12),dt/12,MW)
+
+
 #RK and observations comparison
 plt.figure(figsize=[15,10])
 plt.title(substance+' RK4 and observations comparison')
-plt.plot(timebar, oneboxRK4(k,P,C0,dt,MW)*1.07,label='RK4 model')
+plt.plot(timebar, oneboxRK4(k,P,C0,dt,MW)*1.06,label='RK4 model')
 plt.fill_between(timebar, Errormax, Errormin, alpha=0.2)
 plt.plot(O_df['time'],O_df[substance]/(10**12),label='AGAGE observations')
 plt.fill_between(O_df['time'],ObsErrormax/(10**12),ObsErrormin/(10**12),alpha=0.4)
@@ -71,8 +77,8 @@ plt.show()
 #Euler and RK4 comparison
 plt.figure(figsize=[15,10])
 plt.title(substance+' Euler and RK4 comparison')
-plt.plot(timebar, oneboxeulerfw(k,P,C0,dt,MW)*1.07,label='Euler model')
-plt.plot(timebar, oneboxRK4(k,P,C0,dt,MW)*1.07,label='RK4 model')
+plt.plot(timebar, oneboxeulerfw(k,P,C0,dt,MW)*1.06,label='Euler model')
+plt.plot(timebar, oneboxRK4(k,P,C0,dt,MW)*1.06,label='RK4 model')
 plt.plot(O_df['time'],O_df[substance]/(10**12),label='AGAGE observations')
 plt.ticklabel_format(axis='y', scilimits=(-12,-12))
 plt.ylabel(substance+' concentration [ppt]')
@@ -80,13 +86,36 @@ plt.xlabel('year')
 plt.legend()
 plt.show()
 
+
+
+
+P_monthly=inverseonebox(k,O_df[substance]/(10**12),dt/12,MW)
+
+P_yearly=np.zeros(int(len(P_monthly)/12)+1)
+P_ymax=np.zeros(int(len(P_monthly)/12)+1)
+P_ymin=np.zeros(int(len(P_monthly)/12)+1)
+for i in range(int(len(P_monthly)/12+1)):
+    if i ==0:
+        P_yearly[i]=(np.mean(P_monthly[0:5]))
+        P_ymax[i]=(np.mean(Invmax[0:5]))
+        P_ymin[i]=(np.mean(Invmin[0:5]))
+    elif i ==39:
+        P_yearly[i]=(np.mean(P_monthly[474:]))
+        P_ymax[i]=(np.mean(Invmax[474:]))
+        P_ymin[i]=(np.mean(Invmin[474:]))
+    else:    
+        P_yearly[i]=(np.mean(P_monthly[((i-1)*12+5):((i-1)*12+17)]))
+        P_ymax[i]=(np.mean(Invmax[((i-1)*12+5):((i-1)*12+17)]))
+        P_ymin[i]=(np.mean(Invmin[((i-1)*12+5):((i-1)*12+17)]))
+    
+
 #Emissions
 plt.figure(figsize=[15,10])
 plt.title(substance+' emissions')
 plt.plot(timebar, P)
+#plt.plot(O_df['time'][:-1],inverseonebox(k,O_df[substance]/(10**12),dt/12,MW))
+plt.plot(np.linspace(1978.5,2017.5,40),P_yearly)
+plt.fill_between(np.linspace(1978.5,2017.5,40),P_ymin,P_ymax,alpha=0.3,color='#ff7f0e')
 plt.ylabel(substance+' emissions [Mt/yr]')
 plt.xlabel('year')
 plt.show()
-
-
-
