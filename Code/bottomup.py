96446618fcbc1a5b53a4b7082dc0173f09c5f7a8
Reint <33000433+reint-fischer@users.noreply.github.com>
2019-10-25 14:45:22 +0200

    Organise data

--- Code/bottomup.py
+++ Code/bottomup.py
@@ -2,6 +2,7 @@
 """
 Bottom up emissions
 
+From AFEAS data of cumulative sales of CFC-11 products, compute bank sizes and emissions at all levels from production to installation to banks
 Created on Thu Oct 24 15:00:20 2019
 
 @author: reint fischer
@@ -13,30 +14,42 @@ import pandas as pd
 AFEAS_df = pd.read_excel('em_cfc_11')
 
 ### Emission rates ###
-rPnD = 0.015
-riRAC = 0.100
-riClosed = 0.300
-riOpen = 0.980
-rRAC = 0.040
-rClosed = 0.100
-rOpen = 0.700
+rPnD = 0.015 #Production & Distribution
+riRAC = 0.100 #Refrigeration and Air Conditioning Installation
+riClosed = 0.300 #Closed Foams Installation
+riOpen = 0.980 #Open Foams and Emissive Uses Installation
+rRAC = 0.040 #Refrigeration and Air Conditioning Banks
+rClosed = 0.100 #Closed Foams Banks
+rOpen = 0.700 #Open Foams and Emissive Uses Banks
 
-### Budgets ###
+### Cumulative sales ###
 RAC = AFEAS_df['S_rnonhermetic']+AFEAS_df['S_rhermetic']
 Closed = AFEAS_df['S_closed']
 Open = AFEAS_df['S_open']
 
-### Production and Distribution ###
-PRAC1 = RAC*rPnD
-PCLO1 = Closed*rPnD
-POPE1 = Open*rPnD
+### Production and Distribution Emissions ###
+PRAC1 = []
+PCLO1 = []
+POPE1 = []
+for i in range(len(RAC)-1):
+    PRAC1 += (RAC[i+1]-RAC[i])*rPnD
+    PCLO1 += (Closed[i+1]-Closed[i])*rPnD
+    POPE1 += (Open[i+1]-Open[i])*rPnD
 
+#Leftover after production
 RAC = RAC - PRAC1
 Closed = Closed - PCLO1
 Open = Open - POPE1
 
 ### Installation ###
+PRAC2 = RAC*riRAC
+PCLO2 = Closed*riClosed
+POPE2 = Open*riOpen
 
+#Leftover after production
+RAC = RAC - PRAC1
+Closed = Closed - PCLO1
+Open = Open - POPE1
 
 ### Banks release ###
 
